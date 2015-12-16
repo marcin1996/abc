284965984cf98928323ecb68be66c528b57bc26d
marcin1996 <marcin.j96@wp.pl>
2015-12-16 21:37:06 +0100

    Update kod2.cpp

--- kod2.cpp
+++ kod2.cpp
@@ -8,6 +8,7 @@ const size=200;   //Ilosc liter w zdaniu - dlugosc zdania
 char string[size];
 cout<<"Wpisz zadanie max "<<size<<" literowe :";
 cin>>string;
+cout<<string;
 
 for (int i=0,ilosc=0;i<size;i++)
 	  {
